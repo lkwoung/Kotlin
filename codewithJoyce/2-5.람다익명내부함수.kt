bbe464b7601472039c6de47744e21896b05746a0
lkwoung <whrjzm123@gmail.com>
2022-05-29 08:47:40 +0900

    0529 2강

---
+++ codewithJoyce/2-5.람다익명내부함수.kt
@@ -0,0 +1,23 @@
+package com.lkwoung.kotlinpractice
+
+//람다함수 - 익명내부함수
+// 1. Kotlin Interface 가 아닌 자바 인터페이스여야 합니다.
+// 2. 그 인터페이스는 딱 하나의 메소드만 가져야한다.
+/*
+    button.setOnClickListener(object : View.OnClickListener{
+        override fun onClick(p0 : View?){
+
+        }
+    })
+
+    // 위 함수와 아래 함수가 같음
+    // setOnClickListener는 자바 인터페이스
+    // 그 인터페이스는 OnClickListener 단 하나의 메소드만 가짐
+    **
+    button.setOnClickListener{
+           // to do...
+    }
+ */
+fun main(){
+
+}
